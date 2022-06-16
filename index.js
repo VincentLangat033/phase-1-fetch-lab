d6a09b4befced6564ef812bdc4646884cba0f373
Vincent Langat <kimutailangat033@gmail.com>
2022-06-16 17:34:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Ensure that you return fech(URL)
+// on line 6 we pass a function
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
     .then(resp => resp.json())
